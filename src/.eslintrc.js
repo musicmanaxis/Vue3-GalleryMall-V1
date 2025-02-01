cd79cd61837535760f3a49b57de5548e10419f47
Axis <erlia@naver.com>
2025-02-01 13:26:54 +0900

    1

---
+++ src/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        'vue/no-unused-components': 'off',
+        'vue/no-multiple-template-root': 'off',
+        'vue/no-reserved-component-names': 'off',
+        'vue/multi-word-component-names': 'off',
+    }
+    
+};
